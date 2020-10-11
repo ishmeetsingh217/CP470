e1f957f5139817a9ee43d2c833ed23fd125b4c78
Ishmeet Singh <ishmeetsingh217@gmail.com>
2020-10-11 18:13:52 -0400

    Added 3 activies for part 2

--- app/src/main/java/com/example/android/androidassignments/LoginActivity.java
+++ app/src/main/java/com/example/android/androidassignments/LoginActivity.java
@@ -2,7 +2,9 @@ package com.example.android.androidassignments;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +12,45 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Log.i(msg:"onCreate")
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+    }
+    protected static final String ACTIVITY_NAME = "MainActivity";
+
+
+
+
+
 }
