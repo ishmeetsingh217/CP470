37deff6ff910229af3d5c71f8011672a9ca6a0de
Ishmeet Singh <ishmeetsingh217@gmail.com>
2020-11-17 02:51:25 -0500

    Finished Assignment3Part 2

--- app/src/main/java/com/example/android/androidassignments/TestToolbar.java
+++ app/src/main/java/com/example/android/androidassignments/TestToolbar.java
@@ -88,7 +88,7 @@ public class TestToolbar extends AppCompatActivity {
                     @Override
                     public void onClick(View v){
 
-                        startActivity(moveIntent);
+                       mmDialog.hide();
 
                     }
                 });
